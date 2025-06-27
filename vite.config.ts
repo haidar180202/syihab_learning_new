e3f1c698b0df06b3248346e9ba9b30e35e0894d5
haidar180202 <gshaidar6@gmail.com>
2025-06-27 22:27:35 +0700

    feat(vite): update vite config and update some problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 800, // Naikkan limit peringatan menjadi 800 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Pisahkan vendor besar
+          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
+          reactVendor: ["react", "react-dom"]
+        }
+      }
+    }
+  }
+});
